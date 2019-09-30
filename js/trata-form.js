a1ba134131be0d8bf31b59ce204c7eaf5778be0b
Gustavo Wagner Diniz Mendes <gugawag@gmail.com>
2019-09-30 12:38:31 -0300

    inicio2

--- js/trata-form.js
+++ js/trata-form.js
@@ -1,13 +1,10 @@
-
 var botao = document.querySelector("#btEnviar");
 
-botao.addEventListener("click", function(evento) {
+botao.addEventListener("click", function (evento) {
     var idade = document.querySelector("#idade").value;
     evento.preventDefault();
-    console.log(typeof idade)
-    console.log(idade)
     if (idade >= 18) {
-        console.log("ok");
+        alert("Tudo certo!");
     } else {
         alert("Mermão!! Q tá fazendo aqui? Vaza");
     }
